2a5fc0240d2180729da6b6df71a6fd2da7c68f3b
MSonny68 <maik.sonnenberg@hotmail.degit config --global user.name MSonny68git loggit config --global user.email maik.sonnenberg@hotmail.de>
2022-11-14 09:39:46 +0100

    funktionen rechenarten getestet

--- script_01.js
+++ script_01.js
@@ -5,6 +5,55 @@
 1. Dateneingabe + -überprüfung :
 2. Auswahl Rechenart : 
 3. Fkt. Grundrechenarten :
-4. Ausgabe in Konsole :
+4. Ausgabe in Konsole : check!
 */
+
+
+
+// module: multiplikation zahl1 * zahl2 | test
+output(div(4 , 5));
+output(div(2 , -3));
+output(div(2,0));
+function div(zahl1, zahl2) {
+    if (zahl2 == 0) {
+        output("Division durch 0 nicht möglich");
+    }
+    else {
+        return zahl1 / zahl2;
+    }    
+}  
+
+
+// // module: multiplikation zahl1 * zahl2 | test
+// output(mult(4 , 5));
+// output(mult(2 , -3));
+// output(mult(2,0));
+function mult(zahl1, zahl2) {
+    return zahl1 * zahl2;
+}  
+
+
+// // module: addition zahl1+zahl2 | test
+// output(add(4 , 5));
+// output(add(2 , -3));
+// output(add(2,0));
+function add(zahl1, zahl2) {
+    return zahl1 + zahl2;
+}  
+
+// module: subtraktion zahl1 - zahl2
+// output(sub(4 , 5));
+// output(sub(2 , -3));
+// output(sub(2,0));
+function sub(zahl1, zahl2) {
+    return zahl1 - zahl2;
+}
+
+
+// module: console output | test
+// output("Hello");
+// output(2);
+function output(outputData) {
+    console.log(outputData);
+}
 
